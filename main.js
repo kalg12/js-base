4be12e461f8a19f19ae3c61eefc215266a5d62ed
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-02-28 13:01:06 -0600

    Calcular área del rectángulo

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ const calcularAreaCuadrado = () => {
   <h5>El resultado del área del cuadrado es: ${areaCuadrado}</h5>
   `;
 };
+
+//Función para calcular el área de un rectángulo
+const calcularAreaRectangulo = () => {
+  //Vamos a recuperar el valor del input1
+  const valorRectangulo1 = document.getElementById("valorRectangulo1").value;
+  console.log(valorRectangulo1);
+  //Vamos a recuperar el valor del input2
+  const valorRectangulo2 = document.getElementById("valorRectangulo2").value;
+  console.log(valorRectangulo2);
+  //Vamos a sacar la operación matemática para calcular el área del rectángulo
+  const areaRectangulo = valorRectangulo1 * valorRectangulo2;
+  console.log(areaRectangulo);
+  //Necesito llevar el resultado al HTML
+  document.getElementById("resultadoRectangulo").innerHTML = `
+  <h5>El resultado del área del rectángulo es: ${areaRectangulo}</h5>
+  `;
+};
