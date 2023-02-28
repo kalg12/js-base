0063072d5c07711082f4bd9008d09e9c70d3db3c
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-02-28 11:39:48 -0600

    Bootraspt CDN

---
+++ console.log.js
@@ -0,0 +1,18 @@
+// ESCRIBIR => console.log en JavaScript
+console.log("Hola Mundo");
+
+// JS ES UN LENGUAJE DE PROGRAMACIÓN DEBILMENTE TIPADO
+// JS PARA HACER USO DE VARIABLES HACEMOS USO DE: let & const
+
+const myName = "Kevin Armando";
+const myLastName = "Luciano García";
+let myAge = 28;
+let isStudent = false;
+let isTeacher = true;
+
+//PODEMOS CONCATENER VARIABLES Y USARLAS PARA LO QUE DESEEMOS
+console.log(myName);
+//VOY A CONCATENER MI NOMBRE COMPLETO
+console.log(myName + " " + myLastName);
+// ES6 - Concatenar es ahora más fácil
+console.log(`${myName} ${myLastName}`);
